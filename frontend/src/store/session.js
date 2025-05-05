9e9974539ea16a2d3f4522e50556607dc2a52432
Bhandaljag <jaggasingh1215@gmail.com>
2025-05-05 12:59:37 -0700

    Finished Phase 1

---
+++ frontend/src/store/session.js
@@ -0,0 +1,77 @@
+import { csrfFetch } from "./csrf";
+
+const SET_USER = 'session/setUser';
+const REMOVE_USER = 'session/removeUser';
+
+
+//Action creators
+const setUser = (user) => {
+    return {
+    type: SET_USER,
+    payload: user,
+    };
+};
+
+const removeUser = () => {
+    return {
+    type: REMOVE_USER,
+    };
+};
+
+// Thunk for logging in
+export const login = (user) => async (dispatch) => {
+    const { credential, password } = user;
+    const response = await csrfFetch('/api/session', {
+        method: 'POST',
+        body: JSON.stringify({credential, password }),
+    });
+
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+};
+
+// Thunk Restore user 
+export const restoreUser = () => async (dispatch) => {
+    const response = await csrfFetch("/api/session");
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+  };
+
+  // Thunk signup
+  export const signup = (user) => async (dispatch) => {
+    const { username, firstName, lastName, email, password } = user;
+    const response = await csrfFetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({
+        username,
+        firstName,
+        lastName,
+        email,
+        password
+      })
+    });
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+  };
+
+
+// Initial state
+const InitialState = {user: null};
+
+// Reducer
+const sessionReducer = (state = InitialState, action) => {
+    switch (action.type) {
+        case SET_USER:
+            return { ...state, user: action.payload };
+        case REMOVE_USER:
+            return { ...state, user: null };
+        default:
+            return state;
+    
+    }
+};
+
+export default sessionReducer;
