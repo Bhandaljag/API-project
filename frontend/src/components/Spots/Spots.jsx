b9f2c37df7b8a3d7f1eb73ccac2ac585c14c84fc
Bhandaljag <jaggasingh1215@gmail.com>
2025-05-09 09:44:09 -0700

    started working on spots, need to mess with it a bit

---
+++ frontend/src/components/Spots/Spots.jsx
@@ -0,0 +1,28 @@
+import { useEffect} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {fetchSpots} from '../../store/spots';
+import { Link } from 'react-router-dom';
+
+export default function SpotIndex() {
+    const dispatch = useDispatch();
+    const spots = Object.values(useSelector(state => state.spots));
+
+    useEffect (() => {
+        dispatch(fetchSpots());
+    }, [dispatch]);
+
+    return (
+        <div className='spot-list'>
+        {spots.map(spot => (
+            <Link key={spot.id} to={`/spots/${spot.id}`}>
+                <div className='spot-tile'>
+                    <img src={spot.previewImage} alt={spot.name} />
+                    <h3>{spot.name}</h3>
+                    <p>{spot.city}, {spot.state}</p>
+                    <p>${spot.price} / night</p>
+                </div>
+            </Link>
+        ))}
+        </div>
+    );
+}
